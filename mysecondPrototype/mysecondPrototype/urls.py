601a64b81c252b2c574632adac2e0668108ea929
Akhilesh <akhilesh.gairola@rsystems.com>
2018-05-30 14:35:45 +0530

    little changes done

--- mysecondPrototype/mysecondPrototype/urls.py
+++ mysecondPrototype/mysecondPrototype/urls.py
@@ -13,9 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
-
+from django.conf.urls import url
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+url('',views.homepageview,name='home')
 ]
